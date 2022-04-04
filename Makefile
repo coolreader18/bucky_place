623eb787d5f3d3be04787648346fc70afcb35b8b
Noa <coolreader18@gmail.com>
2022-04-04 17:42:58 -0500

    Simplify generator script

--- Makefile
+++ Makefile
@@ -7,16 +7,14 @@ slice = $(wordlist $1,$(words $2),$2)
 OVERLAYS :=
 
 define overlay =
-OVERLAYS += build/$1_overlay.png
-build/$1_overlay.png: $1.png
-	@mkdir -p $$(@D)
-	python ./generator.py $$< $$@ $2 $3
-	optipng $$@
+IMGS += $1.png
+OVERLAY_CMDS += --template $1.png $2 $3
 endef
 
 $(eval $(call overlay,chi,594,399))
 $(eval $(call overlay,bucky_face,250,1552))
 $(eval $(call overlay,wisco,99,579))
 
-bucky_overlay.png: $(OVERLAYS)
-	convert $< $(foreach f,$(call slice,2,$^),$f -composite) $@
+bucky_overlay.png: $(IMGS)
+	python ./generator.py $@ $(OVERLAY_CMDS)
+	optipng $@
--- generator.py
+++ generator.py
@@ -1,67 +1,40 @@
-# modified from https://github.com/OwO-Yuki-UwU/r_place_overlay_generator/
-
 from PIL import Image
-
-import sys
-image_file_name = sys.argv[1]
-output_overlay_image_name = sys.argv[2]
-image_place_location_x = int(sys.argv[3])
-image_place_location_y = int(sys.argv[4])
-canvas_width, canvas_height = 2000, 2000
+from itertools import product
 
 
+def convert_to_overlay(canvas_dim, images):
+    cw, ch = canvas_dim
+    canvas_dim = cw * 3, ch * 3
+
+    overlay_img = Image.new(mode="RGBA", size=canvas_dim, color=(0, 0, 0, 0))
+
+    def adjusted(location, length):
+        return map(lambda a: (a, (location + a) * 3 + 1), range(length))
+
+    for place_img, location in images:
+        lx, ly = location
+        for (x, x2), (y, y2) in product(
+            adjusted(lx, place_img.width),
+            adjusted(ly, place_img.height),
+        ):
+            overlay_img.putpixel((x2, y2), place_img.getpixel((x, y)))
+
+    return overlay_img
 
 
-overlay_img = Image.new(mode = "RGBA", size = (canvas_width * 3, canvas_height * 3), color = (0, 0, 0, 0))
+if __name__ == "__main__":
+    import argparse
 
-place_img = Image.open(image_file_name)
-place_img = place_img.resize((place_img.width * 3, place_img.height * 3), resample = Image.NEAREST)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("out")
 
-overlay_img.paste(place_img, (image_place_location_x * 3, image_place_location_y * 3))
+    parser.add_argument(
+        "--template", metavar=("IMG", "X", "Y"), nargs=3, action="append"
+    )
 
-new_image_place_location_x = image_place_location_x * 3
-new_image_place_location_y = image_place_location_y * 3
+    args = parser.parse_args()
 
-# Remove other lines
-x = new_image_place_location_x
-y = new_image_place_location_y
-first_remove = True
-while y < new_image_place_location_y + place_img.height:
-    while x < new_image_place_location_x + place_img.width: # Remove line
-        overlay_img.putpixel((x, y), (0, 0, 0, 0))
-        x += 1
+    images = [(Image.open(path), (int(x), int(y))) for path, x, y in args.template]
 
-    if first_remove == False:
-        x = new_image_place_location_x
-        y += 1
-
-        while x < new_image_place_location_x + place_img.width: # Remove line
-            overlay_img.putpixel((x, y), (0, 0, 0, 0))
-            x += 1
-
-    x = new_image_place_location_x
-    y += 2
-    first_remove = False
-
-# Remove other collumns
-y = new_image_place_location_y
-x = new_image_place_location_x
-first_remove = True
-while x < new_image_place_location_x + place_img.width:
-    while y < new_image_place_location_y + place_img.height: # Remove collumns
-        overlay_img.putpixel((x, y), (0, 0, 0, 0))
-        y += 1
-
-    if first_remove == False:
-        y = new_image_place_location_y
-        x += 1
-
-        while y < new_image_place_location_y + place_img.height: # Remove collumns
-            overlay_img.putpixel((x, y), (0, 0, 0, 0))
-            y += 1
-
-    y = new_image_place_location_y
-    x += 2
-    first_remove = False
-
-overlay_img.save(output_overlay_image_name)
+    canvas_dim = 2000, 2000
+    convert_to_overlay(canvas_dim, images).save(args.out)
